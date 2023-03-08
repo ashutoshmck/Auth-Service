a8b0e49291a4b39a750f1a6cf07283a0a2a17514
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-03-08 17:44:07 +0530

    fix: response structure

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,32 +3,32 @@ const HTTPError = require('../utils/errors/http_errors');
 const createUser = async (request, response) => {
   try {
     const user = await UserService.createUser(request.body.username, request.body.password);
-    return response.status(201).json({ status: 201, data: user, message: 'Succesfully Created User' });
+    return response.status(201).json(user);
   } catch (error) {
     if (error instanceof HTTPError)
-      return response.status(error.code).json({ status: error.code, message: error.message });
-    return response.status(500).json({ status: 500, message: error.message });
+      return response.status(error.code).json(error.message);
+    return response.status(500).json(error.message);
   }
 };
 const loginUser = async (request, response) => {
   try {
     const user = await UserService.loginUser(request.body.username, request.body.password);
-    return response.status(200).json({ status: 200, data: user, message: 'Succesfully Logged in' });
+    return response.status(200).json(user);
   } catch (error) {
     if (error instanceof HTTPError)
-      return response.status(error.code).json({ status: error.code, message: error.message });
-    return response.status(500).json({ status: 500, message: error.message });
+      return response.status(error.code).json(error.message);
+    return response.status(500).json(error.message);
   }
 };
 const checkValidityOfToken = async (request, response) => {
   try {
     const token = request.headers.authorization.split(' ')[1];
     await UserService.checkValidityOfToken(token);
-    return response.status(200).json({ status: 200, message: 'Token Verified' });
+    return response.status(200).json('Token Verified');
   } catch (error) {
     if (error instanceof HTTPError)
-      return response.status(error.code).json({ status: error.code, message: error.message });
-    return response.status(500).json({ status: 500, message: error.message });
+      return response.status(error.code).json(error.message);
+    return response.status(500).json(error.message);
   }
 };
 module.exports = { createUser, loginUser, checkValidityOfToken };
